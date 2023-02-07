c3bf8f18b86e76a8c61ea620950fe8b6160fa786
annsilan <intimissiv@mail.ru>
2023-02-07 20:05:39 +0300

    карточки тура

--- src/script.js
+++ src/script.js
@@ -1,52 +1,117 @@
+import { format } from "date-fns"
+const { ru } = require("date-fns/locale")
 
-let tours = [{
-        id: "",
-        country: "",
-        city: "",
-        hotelName: "",
-        image: "",
-        price: "",
-        startTime: "",
-        endTime: "",
-        text: ""
-    } 
-
-]
-
-
-
-async function myTour() {
+async function loadTour() {
     const response = await fetch(
         "https://www.bit-by-bit.ru/api/student-projects/tours"
     )
-    const tours = await response.json()
+    const data = await response.json()
+    return data
+}
 
-    console.log(tours)
-
+function renderTours(tours) {
+    document.getElementById("moretour").innerHTML = ""
     tours.forEach((tour) => {
+        let check
+        if (tour.city && tour.city.length > 0) {
+            check = tour.city + "," + tour.country
+        } else {
+            check = tour.country
+        }
+
+        const startDate = format(new Date(tour.startTime), "dd MMMM yyyy", {
+            locale: ru
+        })
+
+        const endDate = format(new Date(tour.endTime), "dd MMMM yyyy", {
+            locale: ru
+        })
+
         document.getElementById("moretour").innerHTML += `
-        <div class="max-w-4xl mx-auto">
-            <div class="shadow-lg py-3 px-4 rounded-md bg-slate-100 "> 
-                <p class="text-center text-slate-700 text-xl font-bold">${tour.city}</p>
-                <p class="text-center text-slate-700 text-xl font-bold ">${tour.country}</p>
-                
-                <p class="text-slate-500 text-center mt-5">${tour.hotelName}</p>
-                <div class="flex flex-wrap justify-center text-slate-500 ">
-                    <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-yellow-500">
-                    <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                    </svg>
-                    <p> ${tour.rating}</p>
+           <div class="max-w-4xl mx-auto ">
+             <div class="shadow-lg  rounded-md bg-slate-100 flex flex-wrap grid grid-cols-2"> 
+                <img class="rounded-l-md" src="${tour.image}"  />
+                <div class="flex flex-col justify-between mt-5 mb-5">
+                    <div>
+                      <p class="text-slate-700 text-xl font-bold ml-5 ">${tour.hotelName}</p>
+                        <div class="flex flex-wrap text-slate-500 text-xl ml-5 ">
+                           <p>${check}</p>
+                        </div>
+                    </div>
+                <div class="flex flex-wrap justify-between text-slate-500 mr-2 mt-8 ">
+                <p class="text-slate-700 text-xl font-bold ml-5"> ${tour.price} руб</p>
+                    <p class="rounded-md bg-orange-500 p-1 text-slate-100"> ${tour.rating}</p>
                 </div>
-                <img src="${tour.image}" class="mt-5" />
-                <p class="text-slate-700 text-xl font-bold mt-5"> ${tour.price}</p>
-                <div>
-                    <p>${tour.startTime}</p>
-                    <p> ${tour.endTime}</p>
+                <div class="text-slate-600 ml-5 mt-9">
+                   <p >${startDate} - ${endDate}</p>
                 </div>
-                
+                <div class="flex flex-wrap justify-end mr-2">
+                <button
+                        class="w-14 ml-5 font-medium text-slate-600 py-3 px-4  hover:text-amber-600 hover:scale-150 transition-all duration-300 ease-linear"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                            />
+                        </svg>
+                    </button></div>
+                </div>
+               
             </div>
-        
-        </div>`
+            `
     })
 }
-myTour()
+
+async function init() {
+    const tours = await loadTour()
+    renderTours(tours)
+}
+init()
+
+//кнопка добавления списка туров
+let tourInfo = document.getElementById("more")
+
+const btnmore = document.getElementById("addTour")
+btnmore.addEventListener("click", add)
+
+function add() {
+    tourInfo.style.display = "flex"
+    renderTours(tours)
+}
+
+//кнопка вверх
+const btnUp = {
+    el: document.querySelector(".btn-up"),
+    addEventListener() {
+        document.querySelector(".btn-up").onclick = () => {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: "smooth"
+            })
+        }
+    }
+}
+btnUp.addEventListener()
+
+//закрытие списка туров
+const btnClose = document.getElementById("btn-close")
+btnClose.addEventListener("click", close)
+
+function close() {
+    tourInfo.style.display = "none"
+}
+
+let mycity = document.getElementById("city")
+if (mycity === null) {
+    delete mycity.innerHTML
+}
