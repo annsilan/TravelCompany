378b14c6dd067f201058e9620b64767d73c6a14d
annsilan <intimissiv@mail.ru>
2023-02-27 11:04:32 +0300

    формат

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import { format } from "date-fns"
+import { format, max, min } from "date-fns"
 const { ru } = require("date-fns/locale")
 
 async function loadTour() {
@@ -46,10 +46,10 @@ function renderTours(tours) {
                    <p >${startDate} - ${endDate}</p>
                 </div>
                 <div class="flex flex-wrap justify-end mr-2">
-                <button
+                <button onclick="like()" id="noneLike"
                         class="w-14 ml-5 font-medium text-slate-600 py-3 px-4  hover:text-amber-600 hover:scale-150 transition-all duration-300 ease-linear"
                     >
-                        <svg
+                        <svg id="noLike"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
@@ -63,7 +63,10 @@ function renderTours(tours) {
                                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
                             />
                         </svg>
+                        <button class="text-slate-600 font-medium border border-solid py-3 px-4 rounded-md bg-gradient-to-r from-slate-100 to-slate-200 hover:text-amber-600 transition-all duration-300 ease-linear">Забронировать</button>
+
                     </button></div>
+                    
                 </div>
                
             </div>
@@ -71,9 +74,98 @@ function renderTours(tours) {
     })
 }
 
+
+
+// фильтр по стране
+function filterCountry(tours, country) {
+    if (country) {
+        const filterTours = tours.filter((tour) => {
+            return tour.country === country
+        })
+        renderTours(filterTours)
+    } else {
+        renderTours(tours)
+    }
+}
+
+// сортировка по цене
+
+function sortPriceMin(tours) {
+    tours.sort(function (a, b) {
+        return parseFloat(b.price) - parseFloat(a.price)
+    })
+    renderTours(tours)
+}
+function sortPricemMax(tours) {
+    tours.sort((a, b) => parseFloat(a.price) - parseFloat(b.price))
+
+    renderTours(tours)
+}
+
+// // фильтр по рейтингу
+// function filterRaiting (tours,rating){
+
+//     if (rating>=8){
+
+//        const filterTours = tours.filter((tour)=>{
+//             return tour.rating===rating
+//         })
+//         renderTours(filterTours)
+//     } else {
+//         renderTours(tours)
+//     }
+// }
+
 async function init() {
     const tours = await loadTour()
     renderTours(tours)
+    //  страны
+    document
+        .getElementById("Tailand")
+        .addEventListener("click", () => filterCountry(tours, "Тайланд"))
+    document
+        .getElementById("Egypt")
+        .addEventListener("click", () => filterCountry(tours, "Египет"))
+    document
+        .getElementById("Cyprus")
+        .addEventListener("click", () => filterCountry(tours, "Кипр"))
+    document
+        .getElementById("Maldives")
+        .addEventListener("click", () => filterCountry(tours, "Мальдивы"))
+    document
+        .getElementById("Indonesia")
+        .addEventListener("click", () => filterCountry(tours, "Индонезия"))
+    document
+        .getElementById("Mexico")
+        .addEventListener("click", () => filterCountry(tours, "Мексика"))
+    document
+        .getElementById("Tanzania")
+        .addEventListener("click", () => filterCountry(tours, "Танзания"))
+    document
+        .getElementById("all")
+        .addEventListener("click", () => filterCountry(tours))
+
+    // цена
+    document
+        .getElementById("price-min")
+        .addEventListener("click", () => sortPriceMin(tours, "По убыванию"))
+    document
+        .getElementById("price-max")
+        .addEventListener("click", () => sortPricemMax(tours, "По возрастанию"))
+
+    // рейтинг
+    document
+        .getElementById("rating-1")
+        .addEventListener("click", () => filterRaiting(tours, "7 и выше"))
+    document
+        .getElementById("rating-2")
+        .addEventListener("click", () => filterRaiting(tours, "8 и выше"))
+    document
+        .getElementById("rating-3")
+        .addEventListener("click", () => filterRaiting(tours, "9 и выше"))
+    document
+        .getElementById("rating-all")
+        .addEventListener("click", () => filterRaiting(tours))
 }
 init()
 
@@ -111,7 +203,64 @@ function close() {
     tourInfo.style.display = "none"
 }
 
-let mycity = document.getElementById("city")
-if (mycity === null) {
-    delete mycity.innerHTML
+//Меню страна
+
+const dropdownBtn = document.getElementById("dropdownButton")
+const dropdownMenu = document.getElementById("dropdownMenu")
+const toggleArrow = document.getElementById("arrow")
+
+const toggleDropdown = function () {
+    dropdownMenu.classList.toggle("show")
+    toggleArrow.classList.toggle("arrow")
 }
+
+dropdownBtn.addEventListener("click", function (e) {
+    e.stopPropagation()
+    toggleDropdown()
+})
+
+document.documentElement.addEventListener("click", function () {
+    if (dropdownMenu.classList.contains("show")) {
+        toggleDropdown()
+    }
+})
+
+// Меню  цена
+const dropdownСheckboxBtn = document.getElementById("dropdownRadioButton")
+const dropdownСheckboxMenu = document.getElementById("dropdownRadio")
+const togglePrice = document.getElementById("price")
+
+const toggleDropdownСheckbox = function () {
+    dropdownСheckboxMenu.classList.toggle("show")
+    togglePrice.classList.toggle("price")
+}
+
+dropdownСheckboxBtn.addEventListener("click", function (x) {
+    x.stopPropagation()
+    toggleDropdownСheckbox()
+})
+
+document.documentElement.addEventListener("click", function () {
+    if (dropdownСheckboxMenu.classList.contains("show")) {
+    }
+})
+
+// Меню  рейтинг
+const dropdownRatingBtn = document.getElementById("dropdownRatingButton")
+const dropdownRatingMenu = document.getElementById("dropdownRatingbox")
+const toggleArrowRating = document.getElementById("arrowRating")
+
+const toggleDropdownRating = function () {
+    dropdownRatingMenu.classList.toggle("show")
+    toggleArrowRating.classList.toggle("arrowRating")
+}
+
+dropdownRatingBtn.addEventListener("click", function (y) {
+    y.stopPropagation()
+    toggleDropdownRating()
+})
+
+document.documentElement.addEventListener("click", function () {
+    if (dropdownRatingMenu.classList.contains("show")) {
+    }
+})
